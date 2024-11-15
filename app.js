ac344d39beff1bdcbbc17cd71e347f0887aaadf1
Mike <michaelmartins.js@hotmail.com>
2024-11-14 23:06:33 -0300

    add CORS library and Configs

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 // App core File Configuration
 
 const express = require('express')
+const cors = require('cors');
 const app = express();
 
 // const path = require('path');
+
+// TEMPORARY !! --- DANGER -- REMOVE IN PRODUCTION
+app.use(cors());
+
+
+// app.use(cors({
+//     origin: 'http://seu-frontend.com', // Origem permitida (por exemplo, o domínio do seu frontend)
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
+//     allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+// }));
 
 // Enable Express to understand Json
 app.use(express.json());
