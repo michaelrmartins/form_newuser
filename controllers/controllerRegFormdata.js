5bcc26d2cc32b7950aa71655db0c47a8bb90732c
Mike <michaelmartins.js@hotmail.com>
2024-11-22 16:55:52 -0300

    Add Put and Delete

--- controllers/controllerRegFormdata.js
+++ controllers/controllerRegFormdata.js
@@ -2,6 +2,8 @@
 
 const formHandler = require('../models/modelRegDataHandler')
 const CreateUpdateQuery = require('../models/modelDinamicCreateUpdateQuery')
+const { modelUpdateUser } = require('../models/modelUpdateDataHandler')
+const { modelDeleteUser } = require('../models/modelDeleteDataHandler')
 
 // Register new user
 const regUser = async (req, resp ) => {
@@ -11,20 +13,21 @@ const regUser = async (req, resp ) => {
    //    console.log("Valor enviado foi 7")
    // }
    console.log(req.body)
-   const { USERNAME, BIRTHDATE, CPF, PHONE, CEP, ID_ROLE, USER_EXAMPLE } = req.body;
+   const { USERNAME, BIRTHDATE, CPF, PHONE, EMAIL, CEP, ID_ROLE, USER_EXAMPLE } = req.body;
    const result = await formHandler({
        username: USERNAME,
-       birthdate: BIRTHDATE,
-       cpf: CPF,   
-       phone: PHONE,
-       cep: CEP,
+       birthdate: BIRTHDATE || null,
+       cpf: CPF,  
+       phone: PHONE || null,
+       email: EMAIL || null,
+       cep: CEP || null,
        id_role: ID_ROLE,
-       user_example: USER_EXAMPLE
+       user_example: USER_EXAMPLE || null
    });
    return resp.status(201).json({message: `message:User Registred Successfully`, result})
 } catch(error) {
    //console.log(result)
-   console.log("Controller - regUse error:", error);
+   console.log("Controller - regUse error:", error, req.body);
    return resp.status(500).json({message: "Error Registring user", error})
 } // try finally
 
@@ -39,14 +42,30 @@ const updateUser = async (req, resp) =>{
       const condition = {ID: userId}
 
       const { query, values } = CreateUpdateQuery('form_data', data, condition)
+      
+      result = await modelUpdateUser(query, values)
+      console.log(result)
+      return resp.status(200).send({message: "Sucess, updated"});
 
-      console.log(query)
-      await database.execute(query, values )
-      return res.status(200).send({message: "Sucess, updated"});
   } // End Try
   catch (error) {console.log(error); return resp.status(500).send({error: "Update Error"})}
 
 }; // End updateUser
 
+const deleteUser = async (req, resp) => {
+  try {
+    const userId = req.params.id
+    console.log(userId)
+    result = await modelDeleteUser(userId)
+    return resp.status(200).send({message: "Sucess, removed!"})
+
+  } // End Try
+  catch (error) {console.log(error); return resp.status(500).send({error: "Delete Error"})}
+
+
+
+}; // End deleteUser
+
+
 // Exports
-module.exports = { regUser, updateUser };
+module.exports = { regUser, updateUser, deleteUser };
