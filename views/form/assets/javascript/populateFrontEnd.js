79cbfc51201d54a0d09576be289f913543b56073
Mike <michaelmartins.js@hotmail.com>
2024-11-28 22:59:16 -0300

    Minor code fix

--- views/form/assets/javascript/populateFrontEnd.js
+++ views/form/assets/javascript/populateFrontEnd.js
@@ -1,24 +1,22 @@
 async function populateDropdown() {
     try{
-
         // Get data from API
         const roleDataFromDatabase = await fetch('http://192.168.2.214:8059/api/roles');
         const roleValues = await roleDataFromDatabase.json();
 
-        console.log(roleValues);
+        // console.log(roleValues);
 
         roleDropDown = document.getElementById('role-names')
 
         // Write data on Dropdown
         roleValues.forEach( roleValue => {
-            console.log(roleValue.ID)
-            console.log(roleValue.ROLE_NAME)
+            // console.log(roleValue.ID)
+            // console.log(roleValue.ROLE_NAME)
 
             const option = document.createElement('option');
             option.value = roleValue.ID;
             option.textContent = roleValue.ROLE_NAME;
             roleDropDown.appendChild(option)
-           
         });
 
     } // End Try
