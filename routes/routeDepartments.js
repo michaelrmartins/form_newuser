fe502f7733267ef812fe09b5409440ac1163a548
Mike <michaelmartins.js@hotmail.com>
2024-12-16 10:45:37 -0300

    Add post, put and delete Routes

--- routes/routeDepartments.js
+++ routes/routeDepartments.js
@@ -4,19 +4,20 @@ const express = require('express');
 const router = express.Router();
 
 const controllerGetFormData = require('../controllers/controllerGetDepartmentsData')
+const {regDepartment, updateDepartment, deleteDepartment} = require('../controllers/controllerRegDepartmentsData')
 
 // Get's
 router.get("/", controllerGetFormData.getDepartments)
 router.get("/:id", controllerGetFormData.getDepartmentsById)
 
 // Post's
-
+router.post("/", regDepartment)
 
 // Put's
-
+router.put("/:id", updateDepartment)
 
 //Delete's
-
+router.delete("/:id", deleteDepartment)
 
 // Export
 module.exports = router;
