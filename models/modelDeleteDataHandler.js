3ca14e5ed0fcb4bcfaf5f2f774f95540737f44d5
Mike <michaelmartins.js@hotmail.com>
2024-12-16 08:59:50 -0300

    Add modelDeleteRole, modelDeleteDepartments

--- models/modelDeleteDataHandler.js
+++ models/modelDeleteDataHandler.js
@@ -1,9 +1,23 @@
 const database = require('./connections')
-const { queryDeleteUser } = require('./queries/formDeleteData')
+const { queryDeleteUser,
+        queryDeleteRole,
+        queryDeleteDepartments } = require('./queries/formDeleteData')
 
 const modelDeleteUser = async (userId) => {
     const executeReturn = database.execute(queryDeleteUser, [userId])
     return executeReturn;
 };
 
-module.exports = {modelDeleteUser}
+const modelDeleteRole = async (roleId) => {
+    const executeReturn = database.execute(queryDeleteRole, [roleId])
+    return executeReturn;
+};
+
+const modelDeleteDepartments = async (departmentId) => {
+    const executeReturn = database.execute(queryDeleteDepartments, [departmentId])
+    return executeReturn;
+};
+
+module.exports = {modelDeleteUser,
+                  modelDeleteRole,
+                  modelDeleteDepartments};
